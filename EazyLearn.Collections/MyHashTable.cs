eb1a337d986b208e25c112e2e937703853ab1be9
SULFIKKAR E S <sulfikkeres369@hotmail.com>
2022-01-07 23:10:46 +0530

    https://github.com/SulfikkarES

--- EazyLearn.Collections/MyHashTable.cs
+++ EazyLearn.Collections/MyHashTable.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿#region Copyright EazyLearn
+//
+// All rights are reserved. Reproduction or transmission in whole or in part, 
+// in any form or by any means, electronic, mechanical or otherwise, is
+// prohibited without the prior written consent of the copyright owner.
+//
+// Filename        : C#Collections.cs
+// Purpose         : To practice C# Collections 
+// Creation Date   : 20-09-2021
+// Author          : Sulfikkar E S
+//
+// Change History  :
+// Changed by      :              
+// Date            : 
+// Purpose         :
+//
+#endregion Copyright 
+
+#region Included namespaces
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion 
 
 namespace EazyLearn.Collections
 {
